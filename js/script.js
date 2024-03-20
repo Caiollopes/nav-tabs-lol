d8c69138638ff08ef6b99ef4b44213be798e5ac0
Caio Lopes <pess.caio@hotmail.com>
2024-03-19 22:31:51 -0300

    feat: add script type skin

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const tabSelect = document.querySelectorAll('.js-select li');
 const tabBackground = document.querySelectorAll('.js-background li');
 const tabName = document.querySelectorAll('.js-name p');
+const tabSkin = document.querySelectorAll('.js-name span');
 
-if (tabSelect.length && tabBackground.length && tabName.length) {
+
+if (tabSelect.length && tabBackground.length && tabName.length && tabSkin.length) {
     tabBackground[0].classList.add('ativo');
     tabName[0].classList.add('ativo');
+    tabSkin[0].classList.add('ativo');
 
     function activeTab(index) {
         tabBackground.forEach((section) => {
@@ -13,8 +16,12 @@ if (tabSelect.length && tabBackground.length && tabName.length) {
         tabName.forEach((p) => {
             p.classList.remove('ativo');
         });
+        tabSkin.forEach((span) => {
+            span.classList.remove('ativo');
+        });
         tabBackground[index].classList.add('ativo');
         tabName[index].classList.add('ativo');
+        tabSkin[index].classList.add('ativo');
     }
 
     tabSelect.forEach((itemMenu, index) => {
